5320fcd05085b188795bbd6c4bf1049e1f29f5eb
Tanmay <sharnagattanmay@gmail.com>
2024-07-30 00:04:23 +0530

    books schema added

---
+++ backend/models/books.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema ({
+    url: String,
+    filename: String,
+})
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    trim: true,
+    minlength: [1, "Title must be at least 1 character long"],
+    maxlength: [100, "Title cannot exceed 100 characters"],
+  },
+  author: {
+    type: String,
+    required: [true, "Author is required"],
+    trim: true,
+    minlength: [1, "Author name must be at least 1 character long"],
+    maxlength: [50, "Author name cannot exceed 50 characters"],
+  },
+  isbn: {
+    type: String,
+    required: [true, "ISBN is required"],
+    unique: true,
+  },
+  publishedDate: {
+    type: Date,
+    required: [true, "Published date is required"],
+  },
+  genre: {
+    type: [String],
+    required: [true, "Genre is required"],
+  },
+  copiesAvailable: {
+    type: Number,
+    required: [true, "Number of copies available is required"],
+    min: [0, "Number of copies cannot be negative"],
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: [500, "Description cannot exceed 500 characters"],
+  },
+  coverImage: {imageSchema},
+});
+
+export default mongoose.model("Book", bookSchema);
